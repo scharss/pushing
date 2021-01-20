1c98852b6f8fd734eaff318f7809d30533d0e2e5
ermetica <hermeticamaximus@gmail.com>
2021-01-20 00:14:26 -0500

    Upload files

---
+++ firebase.js
@@ -0,0 +1,11 @@
+// Your web app's Firebase configuration
+var firebaseConfig = {
+    apiKey: "AIzaSyCZ5ZlRIvYbt9REvaH7U2BqsC6-tRZpqIQ",
+    authDomain: "push-7a800.firebaseapp.com",
+    projectId: "push-7a800",
+    storageBucket: "push-7a800.appspot.com",
+    messagingSenderId: "1030539270192",
+    appId: "1:1030539270192:web:987a4cf56dd5cbe5f2f5f5"
+};
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
